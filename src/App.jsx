12c710b74174218b12108ef8aef42d91c72a604d
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-04-26 18:46:48 +0300

    Перше завдання готове

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Profile from "./components/Profile"
+import user from './user.json'
+
+// const user = {
+//   "username": "Jacques Gluke",
+//   "tag": "jgluke",
+//   "location": "Ocho Rios, Jamaica",
+//   "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   "stats": {
+//     "followers": 5603,
+//     "views": 4827,
+//     "likes": 1308
+//   }
+// }
+
+
+export default function App() {
+    return <>
+        <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+    </>
+}
